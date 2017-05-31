e4dd32a2f84cd5dbcb9c250d03c30038c5f5c74f
IvoryCandy <deovant@gmail.com>
2017-05-31 18:28:38 -0400

    update Person2

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,9 +1,10 @@
 package edu.gatech.oad.antlab.person;
+import java.util.Random;
 
 /**
  *  A simple class for person 2
  *  returns their name and a
- *  modified string 
+ *  modified string
  *
  * @author Bob
  * @version 1.1
@@ -30,15 +31,23 @@ public class Person2 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 2 put your implementation here
-	  return null;
+        Random rand = new Random();
+        char a[] = input.toCharArray();
+        for( int i=0 ; i<a.length ; i++ ) {
+        int j = rand.nextInt(a.length);
+        char temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+        }
+        return new String(a);
 	}
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
