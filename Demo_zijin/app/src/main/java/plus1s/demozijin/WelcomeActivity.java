9dc7ccde7859ca27cfb788bbdb4541bdd7267579
IvoryCandy <deovant@gmail.com>
2017-05-31 17:38:40 -0400

    updating

--- Demo_zijin/app/src/main/java/plus1s/demozijin/WelcomeActivity.java
+++ Demo_zijin/app/src/main/java/plus1s/demozijin/WelcomeActivity.java
@@ -18,7 +18,7 @@ public class WelcomeActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
