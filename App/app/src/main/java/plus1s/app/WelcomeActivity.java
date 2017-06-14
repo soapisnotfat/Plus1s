dbe164f39171b0fd971df8864ba35953b1c58e02
IvoryCandy <deovant@gmail.com>
2017-06-14 15:14:17 -0400

    upudate funtionality on welcome activity

--- App/app/src/main/java/plus1s/app/WelcomeActivity.java
+++ App/app/src/main/java/plus1s/app/WelcomeActivity.java
@@ -1,7 +1,12 @@
 package plus1s.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        ImageView to_be = (ImageView) findViewById(R.id.imageView2);
+        Button welcome_login = (Button) findViewById(R.id.welcome_login);
+        Button welcome_register = (Button) findViewById(R.id.welcome_register);
+
+        welcome_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent LoginIntent = new Intent(WelcomeActivity.this, LoginActivity.class);
+                WelcomeActivity.this.startActivity(LoginIntent);
+            }
+        });
+
+        welcome_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent RegisterIntent = new Intent(WelcomeActivity.this, RegisterActivity.class);
+                WelcomeActivity.this.startActivity(RegisterIntent);
+            }
+        });
+
     }
 }
