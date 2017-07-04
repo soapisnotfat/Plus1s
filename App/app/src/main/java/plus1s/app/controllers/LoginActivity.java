cdac1021565caf655690b136d6ee44f92ff048d8
jzhang866 <stellazhang1118@gmail.com>
2017-07-04 00:22:11 -0400

    database

--- App/app/src/main/java/plus1s/app/controllers/LoginActivity.java
+++ App/app/src/main/java/plus1s/app/controllers/LoginActivity.java
@@ -9,14 +9,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import plus1s.app.R;
 import plus1s.app.model.Account;
+import plus1s.app.model.Administrator;
 import plus1s.app.model.Database;
+import plus1s.app.model.Item;
+import plus1s.app.model.Manager;
+import plus1s.app.model.User;
 import plus1s.app.model.UserDetails;
 
 
 public class LoginActivity extends AppCompatActivity {
     Database db = new Database();
+     DatabaseReference database = FirebaseDatabase.getInstance().getReference("user");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,38 +52,70 @@ public class LoginActivity extends AppCompatActivity {
 
         //login action on clicking login button
         login_login.setOnClickListener(new View.OnClickListener() {
+            String log_password = "";
+
             @Override
             public void onClick(View view) {
                 //parse in input data
                 String log_username = login_username.getText().toString().trim();
-                String log_password = login_password.getText().toString().trim();
+                log_password = login_password.getText().toString().trim();
+                DatabaseReference Ref = database.child(log_username);
+                ValueEventListener e = new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        Account user;
+                        System.out.println("downloadRequest::onDataChange is Called!");
+                        switch (dataSnapshot.child("type").getValue(String.class)) {
+                            case "Administrator":
+                                user = new Administrator();
+                                break;
+                            case "Manager":
+                                user = new Manager();
+                                break;
+                            default:
+                                user = new User();
+                                break;
+                        }
 
-                Account online_user = db.downloadRequest(log_username);
-                if (online_user != null) {
-                    if (log_password.equals(online_user.getPassword())) {
-                        UserDetails.login(online_user);
-                        goToMain();
-                    } else {
-                        //display an alert while password is invalid
-                        AlertDialog.Builder dialog3 = new AlertDialog.Builder(LoginActivity.this);
-                        dialog3.setTitle("Invalid Login Attempt");
-                        dialog3.setMessage("please enter correct combination ")
-                                .setNegativeButton("Retry", null)
-                                .create()
-                                .show();
-                        login_password.setText("");
+                        user.setUsername(dataSnapshot.child("username").getValue(String.class));
+                        user.setName(dataSnapshot.child("name").getValue(String.class));
+                        user.setPassword(dataSnapshot.child("password").getValue(String.class));
+                        user.setEmail(dataSnapshot.child("email").getValue(String.class));
+                        user.setIsLocked(dataSnapshot.child("isLocked").getValue(boolean.class));
+                        //GenericTypeIndicator<ArrayList<Item>> t = new GenericTypeIndicator<ArrayList<Item>>() {};
+                        //user.setLostItem(dataSnapshot.child("lostItem").getValue(t));
+                        if (user != null) {
+                            if (log_password.equals(user.getPassword())) {
+                                UserDetails.login(user);
+                                goToMain();
+                            } else {
+                                //display an alert while password is invalid
+                                AlertDialog.Builder dialog3 = new AlertDialog.Builder(LoginActivity.this);
+                                dialog3.setTitle("Invalid Login Attempt");
+                                dialog3.setMessage("please enter correct combination ")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
+                                login_password.setText("");
+                            }
+                        } else {
+                            //display an alert while username is not found
+                            AlertDialog.Builder dialog3 = new AlertDialog.Builder(LoginActivity.this);
+                            dialog3.setTitle("Invalid Login Attempt");
+                            dialog3.setMessage("Username not found")
+                                    .setNegativeButton("Retry", null)
+                                    .create()
+                                    .show();
+                            login_password.setText("");
+                            login_username.setText("");
+                        }
                     }
-                } else {
-                    //display an alert while username is not found
-                    AlertDialog.Builder dialog3 = new AlertDialog.Builder(LoginActivity.this);
-                    dialog3.setTitle("Invalid Login Attempt");
-                    dialog3.setMessage("Username not found")
-                            .setNegativeButton("Retry", null)
-                            .create()
-                            .show();
-                    login_password.setText("");
-                    login_username.setText("");
-                }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                    }
+                };
+                Ref.addListenerForSingleValueEvent(e);
             }
         });
 
