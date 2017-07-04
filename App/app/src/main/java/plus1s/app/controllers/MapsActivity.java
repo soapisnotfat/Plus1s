d0860111bdf975df034c341ce171507181e47615
jzhang866 <stellazhang1118@gmail.com>
2017-07-04 13:40:25 -0400

    map with api key

--- App/app/src/main/java/plus1s/app/controllers/MapsActivity.java
+++ App/app/src/main/java/plus1s/app/controllers/MapsActivity.java
@@ -10,6 +10,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import plus1s.app.R;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
