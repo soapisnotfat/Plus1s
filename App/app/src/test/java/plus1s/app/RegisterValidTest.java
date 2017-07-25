f2b46e650eec40576394f2ae0e80ae8d736c81bc
jzhang866 <stellazhang1118@gmail.com>
2017-07-25 13:33:39 -0400

    register test

--- App/app/src/test/java/plus1s/app/RegisterValidTest.java
+++ App/app/src/test/java/plus1s/app/RegisterValidTest.java
@@ -12,52 +12,73 @@ import static org.junit.Assert.assertTrue;
  */
 public class RegisterValidTest {
     /**
-     * Test for valid register user input
+     * Test for valid name  input
      * @throws Exception if the input is not valid;
      */
     @Test
-    public void registerValidTest() throws Exception {
+    public void nameValidTest() throws Exception {
         assertTrue(RegisterActivity.registerQualified("jzhang866", "jzhang", "shen@", "shen","shen"));
         assertTrue(RegisterActivity.registerQualified("123456", "123456amazon", "shen@", "she","she"));
-    }
-
-    /**
-     * Test for invalid register user input
-     * @throws Exception if the input is not valid;
-     */
-    @Test
-    public void registerInvalidTest() throws Exception {
         assertFalse(RegisterActivity.registerQualified("", "jzhang", "@", "shen","shen"));
-        assertFalse(RegisterActivity.registerQualified("jzhang866", "", "@", "shen","shen"));
-        assertFalse(RegisterActivity.registerQualified("jzhang866", "jzhang", "shen", "shen","shen"));
-        assertFalse(RegisterActivity.registerQualified("jzhang866", "jzhang", "shen@", "sh","sh"));
-        assertFalse(RegisterActivity.registerQualified("jzhang866", "jzhang", "shen@", "shen","sh"));
-        assertFalse(RegisterActivity.registerQualified("jzhang866", "jzhang", "shen", "shen","sheng"));
         assertFalse(RegisterActivity.registerQualified("", "", "shen@", "shen","shen"));
         assertFalse(RegisterActivity.registerQualified("", "jzhang", "shen", "shen","shen"));
         assertFalse(RegisterActivity.registerQualified("", "jzhang", "shen@", "sh","sh"));
         assertFalse(RegisterActivity.registerQualified("", "jzhang", "shen@", "sheng","shen"));
-        assertFalse(RegisterActivity.registerQualified("jzhang866", "", "shen", "shen","shen"));
-        assertFalse(RegisterActivity.registerQualified("jzhang866", "", "shen@", "sh","sh"));
-        assertFalse(RegisterActivity.registerQualified("jzhang866", "", "shen@", "sheng","shen"));
-        assertFalse(RegisterActivity.registerQualified("jzhang866", "jzhang", "shen", "sh","sh"));
-        assertFalse(RegisterActivity.registerQualified("jzhang866", "jzhang", "shen", "sheng","shen"));
-        assertFalse(RegisterActivity.registerQualified("jzhang866", "jzhang", "shen@", "sh","sheng"));
         assertFalse(RegisterActivity.registerQualified("", "", "shen", "shen","shen"));
         assertFalse(RegisterActivity.registerQualified("", "", "shen@", "sh","sh"));
         assertFalse(RegisterActivity.registerQualified("", "", "shen@", "sheng","shen"));
         assertFalse(RegisterActivity.registerQualified("", "jzhang", "shen", "sh","sh"));
         assertFalse(RegisterActivity.registerQualified("", "jzhang", "shen", "sheng","shen"));
         assertFalse(RegisterActivity.registerQualified("", "jzhang", "shen", "sh","sheng"));
-        assertFalse(RegisterActivity.registerQualified("jzhang866", "", "shen", "sh","sh"));
-        assertFalse(RegisterActivity.registerQualified("jzhang866", "", "shen", "sheng","shen"));
-        assertFalse(RegisterActivity.registerQualified("jzhang866", "jzhang", "shen", "sh","sheng"));
         assertFalse(RegisterActivity.registerQualified("", "", "shen", "sh","sh"));
         assertFalse(RegisterActivity.registerQualified("", "", "shen", "sh","sheng"));
         assertFalse(RegisterActivity.registerQualified("", "", "shen", "sh","sheng"));
         assertFalse(RegisterActivity.registerQualified("", "jzhang", "shen", "sh","sheng"));
-        assertFalse(RegisterActivity.registerQualified("jzhang866", "", "shen", "sh","sheng"));
         assertFalse(RegisterActivity.registerQualified("", "", "shen", "sh","sheng"));
 
     }
+    /**
+     * Test for valid username  input
+     * @throws Exception if the input is not valid;
+     */
+    @Test
+    public void usernameValidTest() throws Exception {
+        assertTrue(RegisterActivity.registerQualified("jzhang866", "jzhang", "shen@", "shen","shen"));
+        assertFalse(RegisterActivity.registerQualified("jzhang866", "", "@", "shen","shen"));
+        assertFalse(RegisterActivity.registerQualified("jzhang866", "", "shen", "shen","shen"));
+        assertFalse(RegisterActivity.registerQualified("jzhang866", "", "shen@", "sh","sh"));
+        assertFalse(RegisterActivity.registerQualified("jzhang866", "", "shen@", "sheng","shen"));
+        assertFalse(RegisterActivity.registerQualified("jzhang866", "", "shen", "sh","sh"));
+        assertFalse(RegisterActivity.registerQualified("jzhang866", "", "shen", "sheng","shen"));
+        assertFalse(RegisterActivity.registerQualified("jzhang866", "", "shen", "sh","sheng"));
+    }
+
+    /**
+     * Test for valid email  input
+     * @throws Exception if the input is not valid;
+     */
+    @Test
+    public void emailValidTest() throws Exception {
+        assertTrue(RegisterActivity.registerQualified("jzhang866", "jzhang", "shen@", "shen","shen"));
+        assertTrue(RegisterActivity.registerQualified("123456", "123456amazon", "shen@", "she","she"));
+        assertFalse(RegisterActivity.registerQualified("jzhang866", "jzhang", "shen", "shen","shen"));
+        assertFalse(RegisterActivity.registerQualified("jzhang866", "jzhang", "shen@", "sh","sh"));
+        assertFalse(RegisterActivity.registerQualified("jzhang866", "jzhang", "shen@", "shen","sh"));
+        assertFalse(RegisterActivity.registerQualified("jzhang866", "jzhang", "shen", "shen","sheng"));
+        assertFalse(RegisterActivity.registerQualified("jzhang866", "jzhang", "shen", "sh","sh"));
+        assertFalse(RegisterActivity.registerQualified("jzhang866", "jzhang", "shen", "sheng","shen"));
+        assertFalse(RegisterActivity.registerQualified("jzhang866", "jzhang", "shen@", "sh","sheng"));
+        assertFalse(RegisterActivity.registerQualified("jzhang866", "jzhang", "shen", "sh","sheng"));
+    }
+
+    /**
+     * Test for valid password  input
+     * @throws Exception if the input is not valid;
+     */
+    @Test
+    public void password() throws Exception {
+        assertTrue(RegisterActivity.registerQualified("jzhang866", "jzhang", "shen@", "shen","shen"));
+        assertTrue(RegisterActivity.registerQualified("123456", "123456amazon", "shen@", "she","she"));
+    }
+
 }
