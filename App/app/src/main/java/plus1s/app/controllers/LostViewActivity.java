f1a645378387ab2a2e484e38bf72469608703439
jzhang866 <stellazhang1118@gmail.com>
2017-07-26 01:55:42 -0400

    some animation

--- App/app/src/main/java/plus1s/app/controllers/LostViewActivity.java
+++ App/app/src/main/java/plus1s/app/controllers/LostViewActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -18,15 +20,20 @@ import plus1s.app.model.ItemType;
 
 import plus1s.app.model.LostItem;
 
-public class LostViewActivity extends AppCompatActivity {
+import plus1s.app.model.UserDetails;
 
+
+public class LostViewActivity extends AppCompatActivity {
+    ListView lost_view_list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lost_view);
 
         final TextView lost_view_back = (TextView) findViewById(R.id.lost_view_back);
-        final ListView lost_view_list = (ListView) findViewById(R.id.lost_view_list);
+        lost_view_list = (ListView) findViewById(R.id.lost_view_list);
+        final Animation fade_in_anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
+        lost_view_list.setAnimation(fade_in_anim);
 
         ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, displayLostItem());
         lost_view_list.setAdapter(adapter);
